5458db3817070d09522a20ec5dfd20440eb10bf4
Thando <219037801@student.uj.ac.za>
2023-08-13 14:45:57 +0200

    update the api

--- authentication/database.py
+++ authentication/database.py
@@ -1,17 +1,14 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-
-db_url = os.getenv("CLEARDB_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = ("postgresql://xuvhfjpqhdsymb:e5f2e3e9770dfc1e16880d0061d900dfed08b21d1056fce3b5846e9ca159e1c8@ec2-3-92-151-217.compute-1.amazonaws.com:5432/d2r3pui5sgp38q")
 
 # create sqlalchemy engine "engine"
-
-engine = create_engine(db_url)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,  # Handle dropped connections
+)
 
 # create a session local class - where each instance of this class will represent a database session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def login(email: str, password: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=402, detail="Email not registered")
     if not verify_password(plain_password=password, hashed_password=user.password):
         raise HTTPException(status_code=401, detail="Incorrect password")
-    return {"name": user.name, "surname":user.surname}
+    return user
 
 
 @app.post("/create_cart", response_model=schemas.Cart)
